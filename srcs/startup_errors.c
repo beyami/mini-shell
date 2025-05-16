2a812225d061db6bdec0881097a73c55eb07a94e
stakada <stakada@student.42tokyo.jp>
2025-05-16 17:42:16 +0900

    Fix: startup errors - 起動時に引数が渡された場合のエラーメッセージ修正 - isatty判定にSTDERRを追加

--- srcs/startup_errors.c
+++ srcs/startup_errors.c
@@ -5,11 +5,10 @@ void	handle_startup_errors(int argc, char **argv)
 	(void)argv;
 	if (argc > 1)
 	{
-		write(STDERR_FILENO, "minishell: Non-interactive mode not supported\n",
-			46);
+		write(STDERR_FILENO, "minishell: too many arguments\n", 30);
 		exit(1);
 	}
-	if (!isatty(STDIN_FILENO))
+	if (!isatty(STDIN_FILENO) || !isatty(STDERR_FILENO))
 	{
 		write(STDERR_FILENO, "minishell: stdin is not a tty\n", 30);
 		exit(1);
